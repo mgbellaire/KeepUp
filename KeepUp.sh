c41db8be17141ca83e0ec338a2e0bf557a51c425
Mark Bellaire <mark.bellaire@gmail.com>
2016-07-02 12:29:45 -0400

    TODOs and Reference link
    Added a couple TODOs and a reference link to a bash scripting trick I'm
    using.

--- KeepUp.sh
+++ KeepUp.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
 # TODO: Do the work.
+# TODO: Wrap this in check for "brew" command.
+# Update homebrew packages.
+brew update
+# Upgrade homebrew packages.
+brew upgrade all
+
+if [ -x "$(command -v composer)" ]; then
+# TODO: Update composer packagrs
+fi
