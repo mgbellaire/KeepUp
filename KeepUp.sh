c9c9b8cf0a6f381390e4d9b6138152d5e5a7e428
Mark Bellaire <mark.bellaire@gmail.com>
2016-07-02 12:51:59 -0400

    Closes #1: Update Drupal Coding Standards
    I am also opting for a custom success/error message because the default
    message is not descriptive enough of the goal being achieved by the
    command that is run.

--- KeepUp.sh
+++ KeepUp.sh
@@ -14,3 +14,15 @@ if [ -x "$(command -v composer)" ]; then
   # Update Composer packages that live in ~/.composer.
   composer global update
 fi
+
+# Drupal
+if [ -a ~/.composer/vendor/drupal/coder/coder_sniffer ]; then
+  # Update Drupal Coding Standards.
+  # Suppress any output from command in favor of a custom response for success/failure.
+  phpcs --config-set installed_paths ~/.composer/vendor/drupal/coder/coder_sniffer > /dev/null
+  if [ $? -q 0 ]; then
+    echo 'Drupal Coding Standards were updated successfully.'
+  else
+    echo 'WARNING: Issue updating Drupal Coding Standards. Please run `phpcs --config-set installed_paths ~/.composer/vendor/drupal/coder/coder_sniffer` to debug any errors.'
+  fi
+fi
